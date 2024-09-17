d357d2ba82c625902930d9a7eabf2874c62150a6
vishalmishraa <vishal.mishra9470@gmail.com>
2024-09-17 19:39:36 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -38,8 +38,8 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); // to mak
 
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend/build/index.html'));
-})
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+});
 
 app.use(notFound);  // if we reach this point, it means that the request is not found
 app.use(errorHandler); // if we reach this point, it means that there is an error
