9762f2a3bfdec98d02661d36c7b5876fd20b8f96
vishalmishraa <vishal.mishra9470@gmail.com>
2024-09-17 19:29:19 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'; // to use .env file for that
 dotenv.config();
 
 import connectDB from './config/db.js';
-import { notFound, errorHandler} from './middleware/errorMiddleware.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 //import products from './data/products.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import productRoutes from './routes/productRoutes.js';
@@ -19,30 +19,28 @@ connectDB();
 
 // Body parser midlleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // cookie parser middleware
 
 app.use('/api/products', productRoutes); //send it to productRoutes.js when /api/products is called
-app.use('/api/users', userRoutes); 
+app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 
-app.get('/api/config/paypal', (req, res) => 
-    res.send({clientId: process.env.PAYPAL_CLIENT_ID}) // send the paypal client id to the frontend
+app.get('/api/config/paypal', (req, res) =>
+    res.send({ clientId: process.env.PAYPAL_CLIENT_ID }) // send the paypal client id to the frontend
 );
 
 const __dirname = path.resolve(); // to use __dirname in ES6
-console.log(__dirname); 
+console.log(__dirname);
 app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); // to make the uploads folder static
 
 
-if(process.env.NODE_ENV == 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'frontend/build/index.html')); 
-    })
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend/build/index.html'));
+})
 
-}
 app.use(notFound);  // if we reach this point, it means that the request is not found
 app.use(errorHandler); // if we reach this point, it means that there is an error
 
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 //export const BASE_URL = process.env.NODE_ENV === 'devlopment' ? 'http://localhost:8000': '';
-export const BASE_URL = ''; // If using proxy
+export const BASE_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
